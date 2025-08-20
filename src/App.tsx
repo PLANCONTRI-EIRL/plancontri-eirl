f67a84594fd4ee2d9c694129de764f045e3deeb9
Jeancarlos <jc_26.10@hotmail.com>
2025-08-20 15:11:56 -0500

    Set BrowserRouter basename to /plancontri-eirl
    Configured the React Router's BrowserRouter to use '/plancontri-eirl' as the base path. This change is likely needed for correct routing when the app is deployed under a subdirectory.

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,8 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      {/* <BrowserRouter> */}
+      <BrowserRouter basename="/plancontri-eirl">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
